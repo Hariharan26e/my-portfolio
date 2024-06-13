59418b2ca5fa729d33ac18d368897102cffa8888
hariharan <156800427+Hariharan26e@users.noreply.github.com>
2024-06-13 13:28:27 +0530

    Add files via upload

---
+++ convertor.js
@@ -0,0 +1,42 @@
+let select = document.getElementsByClassName('money')
+
+
+let btn = document.getElementById('btn')
+
+
+let input = document.getElementById('input')
+
+fetch('https://api.frankfurter.app/currencies')
+.then(res=>res.json())
+.then(res=>display(res))
+
+
+function display(res){
+    let curr = Object.entries(res)
+    
+    for(i=0;i<curr.length;i++){
+    let opt =`<option value="${curr[i][0]}">${curr[i][0]}</option>`
+    select[0].innerHTML += opt
+    select[1].innerHTML += opt
+}
+}
+
+btn.addEventListener('click',()=>{
+   let curr1 =  select[0].value
+   let curr2 =  select[1].value
+   let inputVal = input.value
+   if(curr1 === curr2)
+   alert('Enter A Number')
+ else
+ convert(curr1,curr2,inputVal)
+});
+
+
+function convert(curr1,curr2,inputVal){
+    const host = 'api.frankfurter.app';
+fetch(`https://${host}/latest?amount=${inputVal}10&from=${curr1}&to=${curr2}`)
+  .then(resp => resp.json())
+  .then((data) => {
+    document.getElementById('result').value = (Object.values(data.rates)[0])
+  });
+}
